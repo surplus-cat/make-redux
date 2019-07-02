a7df1d92c819e7d62e4481e479c88a2b3f7aae08
Corn <1073406997@qq.com>
2019-07-02 11:19:36 +0800

    add: React-redux：初始化工程

--- src/index.js
+++ src/index.js
@@ -1,87 +1,22 @@
-function createStore (reducer) {
-	let state = null;
-	const listeners = [];
-	const subscribe = (listener) => listeners.push(listener);
-	const getState = () => state;
-	const dispatch = (action) => {
-		state = reducer(state, action); // 覆盖原对象
-		// 用一种通用的方式“监听”数据变化，然后重新渲染页面，这里要用到观察者模式
-		listeners.forEach((listener) => listener());
-	}
-	dispatch({}) // 初始化 state
-	return { getState, dispatch, subscribe };
-}
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import ReactDOM from 'react-dom'
+import Header from './views/Header'
+import Content from './views/Content'
 
-function renderApp(newAppState, oldAppState = {}) { // 防止 oldAppState 没有传入，所以加了默认参数 oldAppState = {}
-	if (newAppState === oldAppState) return // 数据没有变化就不渲染了
-	console.log('render app...')
-	renderTitle(newAppState.title, oldAppState.title);
-	renderContent(newAppState.content, oldAppState.content);
-}
-
-function renderTitle(newTitle, oldTitle = {}) {
-	if (newTitle === oldTitle) return // 数据没有变化就不渲染了
-	console.log('render title...')
-	const titleDOM = document.getElementById('title')
-  titleDOM.innerHTML = newTitle.text
-  titleDOM.style.color = newTitle.color
-}
-
-function renderContent(newContent, oldContent = {}) {
-	if (newContent === oldContent) return // 数据没有变化就不渲染了
-	console.log('render content...')
-	const contentDOM = document.getElementById('content')
-  contentDOM.innerHTML = newContent.text
-  contentDOM.style.color = newContent.color
-}
-
-function stateChanger (state, action) {
-	if (!state) {
-    return {
-      title: {
-        text: 'React.js 小书',
-        color: 'red',
-      },
-      content: {
-        text: 'React.js 小书内容',
-        color: 'blue'
-      }
-    }
-  }
-	switch (action.type) {
-		case 'UPDATE_TITLE_TEXT':
-			return { // 构建新的对象并且返回
-				...state,
-				title: {
-					...state.title,
-					text: action.text
-				}
-			};
-		case 'UPDATE_TITTLE_COLOR':
-			return { // 构建新的对象并且返回
-				...state,
-				title: {
-					...state.title,
-					color: action.color
-				}
-			};
-		default:
-			return state // 没有修改，返回原来的对象
+class Index extends Component {
+	
+	render () {
+		return (
+			<div>
+				<Header />
+				<Content />
+			</div>
+		)
 	}
 }
 
-const store = createStore(stateChanger);
-let oldState = store.getState();
-store.subscribe(() => {
-	const newState = store.getState(); // 数据可能变化，获取新的 state
-	renderApp(newState, oldState);  // 把新旧的 state 传进去渲染
-	oldState = newState; // 渲染完以后，新的 newState 变成了旧的 oldState，等待下一次数据变化重新渲染
-}); // 监听数据变化
-
-renderApp(store.getState()); // 首次渲染页面
-
-store.dispatch({ type: 'UPDATE_TITLE_TEXT', text: '《红楼梦》'}); // 修改标题文本
-store.dispatch({ type: 'UPDATE_TITTLE_COLOR', color: 'pink'}); // 修改标题颜色
-
-// ...后面不管如何 store.dispatch，都不需要重新调用 renderApp
-// renderApp(appState); // 把新的数据渲染到页面上
+ReactDOM.render(
+	<Index />,
+	document.getElementById('root')
+)
---
+++ src/views/Header.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+export default class Header extends Component {
+
+  render() {
+    return (
+      <h1>React.js 小书</h1>
+    )
+  }
+}
