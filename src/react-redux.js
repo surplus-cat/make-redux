ea9d0b40ba717ca2c7d356d0f5223aefabce4f43
Corn <1073406997@qq.com>
2019-07-02 14:12:26 +0800

    add: connect 和 mapStateToProps

---
+++ src/react-redux.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export const connect = (mapStateToProps) => (WrapperComponent) => {
+  class Connect extends Component {
+    static contextTypes = {
+      store: PropTypes.object
+    }
+
+    constructor() {
+      super();
+      this.state = { allProps: {} }
+    }
+
+    componentWillMount () {
+      const { store } = this.context
+      this._updateProps()
+      store.subscribe(() => this._updateProps())
+    }
+
+    _updateProps () {
+      const { store } = this.context;
+      let stateProps = mapStateToProps(store.getState(), this.props); // 额外传入 props，让获取数据更加灵活方便
+      this.setState({
+        allProps: { // 整合普通的 props 和从 state 生成的 props
+          ...stateProps,
+          ...this.props
+        }
+      })
+    }
+
+    render() {
+      
+      // {...xxx} 意思是把这个对象里面的属性全部通过 `props` 方式传递进去
+      return <WrapperComponent {...this.state.allProps} />
+    }
+  }
+
+  return Connect;
+}
--- src/views/Header.js
+++ src/views/Header.js
@@ -1,31 +1,25 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import { connect } from '../react-redux'
 
-export default class Header extends Component {
-  static contextTypes = {
-    store: PropTypes.object
-  }
-
-  constructor() {
-    super();
-    this.state = { themeColor: '' }
-  }
-
-  componentWillMount() {
-    this._updateThemeColor();
-    const { store } = this.context;
-    store.subscribe(() => this._updateThemeColor());
-  }
-
-  _updateThemeColor() {
-    const { store } = this.context;
-    const state = store.getState();
-    this.setState({ themeColor: state.themeColor })
+class Header extends Component {
+  static propTypes = {
+    themeColor: PropTypes.string
   }
 
   render() {
     return (
-      <h1 style={{ color: this.state.themeColor }}>React.js 小书</h1>
+      <h1 style={{ color: this.props.themeColor }}>React.js 小书</h1>
     )
   }
 }
+
+const mapStateToProps = (state) => {
+  return {
+    themeColor: state.themeColor
+  }
+}
+
+Header = connect(mapStateToProps)(Header);
+
+export default Header;
