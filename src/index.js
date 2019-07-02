b76933b2f4ea0c19a2a5cb4099ccffe2982461ec
Corn <1073406997@qq.com>
2019-07-02 13:18:55 +0800

    add: 结合 context 和 store

--- src/index.js
+++ src/index.js
@@ -4,7 +4,45 @@ import ReactDOM from 'react-dom'
 import Header from './views/Header'
 import Content from './views/Content'
 
+function createStore(reducer) {
+  let state = null;
+  const listeners = [];
+  const subscribe = (listener) => listeners.push(listener);
+  const getState = () => state;
+  const dispatch = (action) => {
+    state = reducer(state, action);
+    listeners.forEach((listener) => listener())
+  }
+  dispatch({}); // 初始化 state
+  return { getState, dispatch, subscribe }
+}
+
+const themeReducer = (state, action) => {
+  if (!state) return {
+    themeColor: 'red'
+  }
+
+  switch (action.type) {
+    case 'CHANGE_COLOR':
+      return { ...state, themeColor: action.themeColor }
+    default:
+      return state;
+  }
+}
+
+const store = createStore(themeReducer);
+	
+
 class Index extends Component {
+	static childContextTypes = {
+		store: PropTypes.object
+	}
+	
+
+  getChildContext() {
+    return { store };
+  }
+		
 	
 	render () {
 		return (
--- src/views/Header.js
+++ src/views/Header.js
@@ -2,10 +2,30 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
 export default class Header extends Component {
+  static contextTypes = {
+    store: PropTypes.object
+  }
+
+  constructor() {
+    super();
+    this.state = { themeColor: '' }
+  }
+
+  componentWillMount() {
+    this._updateThemeColor();
+    const { store } = this.context;
+    store.subscribe(() => this._updateThemeColor());
+  }
+
+  _updateThemeColor() {
+    const { store } = this.context;
+    const state = store.getState();
+    this.setState({ themeColor: state.themeColor })
+  }
 
   render() {
     return (
-      <h1>React.js 小书</h1>
+      <h1 style={{ color: this.state.themeColor }}>React.js 小书</h1>
     )
   }
 }
